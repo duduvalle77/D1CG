e16c9c98435bafed93ceaaeb90bc155608f3c984
duduvalle77 <duduvalle77@yahoo.com.br>
2019-09-02 21:25:30 -0300

    trabalho comentado parte de colisao

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <math.h>
-#include <iostream>
 #include "extras.h"
-#define NUMPRISMAS 10
+#define NUMPRISMAS 4
 #include <ctime>
 #include <vector>
 #include <algorithm>
@@ -25,19 +24,30 @@ class triangle
 };
 
 /// Globals
+//variaveis presentes no codigo original
 float zdist = 8.0;
-float rotationX = 0.0, rotationY = 0.0, direction = 0.0;
+float rotationX = 0.0, rotationY = 0.0;
 int   last_x, last_y;
 int   width, height;
-float sphereX = 0, sphereY = -9.5, velX = 0, velY = 0, velInicial = 0.5;
-float desiredFPS = 30;
-bool launched = false , projection = true, colide = false;
-float dx, dy;
-float x = 1.0, y = 1.0, z = 1.0;
-float timet = 0;
+
+float desiredFPS = 60; //define fps do deslocamento da esfera
+float direction = 0.0;//direção do disparador em angulos
+float sphereX = 0, sphereY = -9.5; //variavel que determinam a posição da esfera
+float velX = 0, velY = 0, velInicial = 0.5; //variavel que determinam a velocidade da esfera
+bool launched = false; //variavel que determina se a esfera foi lançada
+bool projection = true; //variavel que determina que tipo de projeção utilizar
+bool colide = false; //variavel que determina se a esfera colidiu com algum prisma
+float dx, dy; //valores de x e y do vetor direção normalizados
+float timet = 0; //variavel que guarda o ultimo instante em que ocorreu colisçao da esfera com um prisma
+float intensidade = 2; //variavel para determinar intensidade da velocidade
+float raioAreaImpacto = 5; //variavel que determina a distancia minima para verificar a possibilidade de colisão da esfera com um prisma
+
+//variaveis utilizadas para definir a posição dos prismas
+float x = 1.0, y = 1.0;
 vertice vBases[NUMPRISMAS][3] = {0};
 vertice vTopos[NUMPRISMAS][3] = {0};
 vertice centros[NUMPRISMAS] = {0};
+
 /// Functions
 void init(void)
 {
@@ -147,8 +157,8 @@ void drawObject(vertice vBase[], vertice vTopo[])
     glEnd();
 
     glBegin(GL_TRIANGLE_FAN);
-        //CalculaNormal(t1, &vetorNormal); // Passa face triangular e endereço do vetor normal de saída
-        //glNormal3f(vetorNormal.x, vetorNormal.y,vetorNormal.z);
+        CalculaNormal(t1, &vetorNormal); // Passa face triangular e endereço do vetor normal de saída
+        glNormal3f(vetorNormal.x, vetorNormal.y,vetorNormal.z);
 
         for(int j = 0; j < 3; j++) // vertices do triangulo
             glVertex3f(t1.v[j].x, t1.v[j].y, t1.v[j].z);
@@ -156,22 +166,23 @@ void drawObject(vertice vBase[], vertice vTopo[])
     glEnd();
 
     glBegin(GL_TRIANGLE_FAN);
-        //CalculaNormal(t2, &vetorNormal); // Passa face triangular e endereço do vetor normal de saída
-        //glNormal3f(vetorNormal.x, vetorNormal.y,vetorNormal.z);
+        CalculaNormal(t2, &vetorNormal); // Passa face triangular e endereço do vetor normal de saída
+        glNormal3f(vetorNormal.x, vetorNormal.y,vetorNormal.z);
         for(int j = 0; j < 3; j++) // vertices do triangulo
             glVertex3f(t2.v[j].x, t2.v[j].y, t2.v[j].z);
         glVertex3f(Lado2[3].x, Lado2[3].y, Lado2[3].z);
     glEnd();
 
     glBegin(GL_TRIANGLE_FAN);
-        //CalculaNormal(t3, &vetorNormal); // Passa face triangular e endereço do vetor normal de saída
-        //glNormal3f(vetorNormal.x, vetorNormal.y,vetorNormal.z);
+        CalculaNormal(t3, &vetorNormal); // Passa face triangular e endereço do vetor normal de saída
+        glNormal3f(vetorNormal.x, vetorNormal.y,vetorNormal.z);
         for(int j = 0; j < 3; j++) // vertices do triangulo
             glVertex3f(t3.v[j].x, t3.v[j].y, t3.v[j].z);
         glVertex3f(Lado3[3].x, Lado3[3].y, Lado3[3].z);
     glEnd();
 }
 
+// Função para desenhar as paredes e a superficie
 void drawEnviroment(void)
 {
         glBegin(GL_TRIANGLE_FAN);
@@ -214,6 +225,7 @@ void drawEnviroment(void)
             glVertex3f(10,-10,1);
         glEnd();
 }
+
 void display(void)
 {
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -221,6 +233,7 @@ void display(void)
     glMatrixMode (GL_PROJECTION);
     glLoadIdentity ();
 
+    //A variavel projection e utilizada para definir se a visão será ortogonal ou em perspectiva, codigo tirado do projection.cpp que esta no material da disciplina
     int ortho = 10;
     if(!projection)
     {
@@ -235,13 +248,16 @@ void display(void)
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+
+    //A variavel projection e utilizada para definir posição da camera
     if(!projection)
         gluLookAt (0.0, 0.0, zdist, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
     else
         gluLookAt (0.0, -18.0, zdist, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-
+    //corDisparador define o valor do segundo parametro de setColor do disparador
     float corDisparador = (3-velInicial)/5;
 
+    //chama as funções de desenhar prismas e ambiente, além de desenhar o disparador e a esfera
     glPushMatrix();
         glRotatef( rotationY, 0.0, 1.0, 0.0 );
         glRotatef( rotationX, 1.0, 0.0, 0.0 );
@@ -254,7 +270,7 @@ void display(void)
             setColor(0,1,0);
             glTranslatef(sphereX,sphereY,0.5);
             glutSolidSphere(0.5,100,100);
-        if(!launched){
+        if(!launched){//verifica se a bola foi lançada para desenhar o disparador
                 glRotatef(direction,0,0,1);
                 setColor(1,corDisparador,0);
                 glTranslatef(0,1.75,0);
@@ -279,16 +295,20 @@ void display(void)
     glutSwapBuffers();
 }
 
+// função para calcular novos valores de dx e dy, a partir do vetor normal(nx,ny,0), sendo o vetor normal normalizado dentro da função
 void calculaVetorNovo(float nx, float ny)
 {
+    //parte para normalizar o vetor normal
     float len_v = sqrt(nx*nx + ny*ny);
     nx /= len_v;
     ny /= len_v;
+    //calcula direção do vetor novo utilizando a lei da reflexão
     float aux = 2*(-dx*nx + -dy*ny);
     dx = nx*aux + dx;
     dy = ny*aux + dy;
 }
 
+//função para detectar e responder a colisão da esfera com as paredes
 void colisionAmbient()
 {
     if(sphereX > 9.5)
@@ -313,60 +333,67 @@ void colisionAmbient()
     }
 }
 
-float distanceSphereToSomething (vertice ponto)
+//função para calcular distancia entre centro da esfera para um determinado vertice
+float distanciaEsferaVertice (vertice ponto)
 {
     float aux = pow(sphereX - ponto.x,2) + pow(sphereY - ponto.y,2);
     return sqrt(aux);
 }
 
-float distance2vertices (vertice ponto1, vertice ponto2)
+//função para calcular distancia entre centro da esfera para um determinado vertice
+float distancia2vertices (vertice ponto1, vertice ponto2)
 {
     float aux = pow(ponto2.x - ponto1.x,2) + pow(ponto2.y - ponto1.y,2);
     return sqrt(aux);
 }
 
-bool inRange (vertice ponto1,vertice ponto2) {
+//função para verificar se a projeção do centro da esfera na linha definido pela aresta do prisma(definido pelos vertices ponto1 e ponto2) estaria na aresta
+//retirado de https://stackoverflow.com/questions/17581738/check-if-a-point-projected-on-a-line-segment-is-not-outside-it
+bool colisaoAresta (vertice ponto1,vertice ponto2) {
     double dx = ponto1.x - ponto2.x;
     double dy = ponto1.y - ponto2.y;
     double innerProduct = (sphereX - ponto2.x)*dx + (sphereY - ponto2.y)*dy;
     return 0 <= innerProduct && innerProduct <= dx*dx + dy*dy;
 }
 
+//função para verificar se a esfera colidiu com a aresta, retirada deste link: https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
 bool colisionSphereLine(vertice ponto1,vertice ponto2)
 {
     bool colidiu = false;
     float aux = fabs((ponto2.y-ponto1.y)*sphereX - (ponto2.x-ponto1.x)*sphereY + ponto2.x*ponto1.y - ponto2.y*ponto1.x);
-    if(aux/distance2vertices(ponto1,ponto2) <= 0.5)
+    if(aux/distancia2vertices(ponto1,ponto2) <= 0.5)
     {
         colidiu = !colidiu;
-        //cout << aux/distance2vertices(ponto1,ponto2) << endl;
     }
     return colidiu ;
 }
 
+//função para verificar se a esfera colidiu com algum prisma
 void colision()
 {
     float nx, ny;
+    //itera para cada prisma
     for(int i = 0; i < NUMPRISMAS; i++)
     {
-        vector< pair <float,int> > vect;// par indice de vertice
-        if(distanceSphereToSomething(centros[i]) < 3.5)
+        vector< pair <float,int> > vect;// par de distancia dos vertices do prisma ao centro da esfera e o indice que o representa em vBase
+        if(distanciaEsferaVertice(centros[i]) < raioAreaImpacto) // se estiver próximo da esfera continua com os testes
         {
             for(int j = 0; j < 3; j++)
             {
-                vect.push_back( make_pair(distanceSphereToSomething(vBases[i][j]),j) );
+                vect.push_back( make_pair(distanciaEsferaVertice(vBases[i][j]),j) );//preenche o vetor vect com as distancias dos vertices do prisma ao centro da esfera e o indice que o representa em vBase
             }
-            sort(vect.begin(), vect.end());
+            sort(vect.begin(), vect.end()); //ordena o vetor
 
-            if(inRange(vBases[i][vect[0].second],vBases[i][vect[1].second]))
+            if(colisaoAresta(vBases[i][vect[0].second],vBases[i][vect[1].second]))//verifica se havera possibilidade de colisao entre a esfera e a aresta dos vertices mais proximos da esfera
             {
-                if(colisionSphereLine(vBases[i][vect[0].second],vBases[i][vect[1].second]))
+                if(colisionSphereLine(vBases[i][vect[0].second],vBases[i][vect[1].second]))//verifica se houve colisão
                 {
+                    //define a normal utilizando o perproduct apresentado nesse link https://www.metanetsoftware.com/technique/tutorialA.html#arvo
                     float ny = -(vBases[i][vect[0].second].x - vBases[i][vect[1].second].x);
                     float nx = vBases[i][vect[0].second].y - vBases[i][vect[1].second].y;
                     vertice auxV1 = {nx+vBases[i][vect[0].second].x,ny+vBases[i][vect[0].second].y,0};
                     vertice auxV2 = {-(nx+vBases[i][vect[0].second].x),-(ny+vBases[i][vect[0].second].y),0};
-                    if(distance2vertices(auxV1,centros[i]) < distance2vertices(auxV2,centros[i]))
+                    if(distancia2vertices(auxV1,centros[i]) < distancia2vertices(auxV2,centros[i]))//esta condição serve para definir o sentido da normal
                     {
                         nx *= -1;
                         ny *= -1;
@@ -377,15 +404,16 @@ void colision()
                     break;
                 }
             }
-            else if(inRange(vBases[i][vect[0].second],vBases[i][vect[2].second]))
+            else if(colisaoAresta(vBases[i][vect[0].second],vBases[i][vect[2].second]))//verifica se havera possibilidade de colisao entre a esfera e a aresta do vertices mais proximo e do mais distante da esfera
             {
-                if(colisionSphereLine(vBases[i][vect[0].second],vBases[i][vect[2].second]))
+                if(colisionSphereLine(vBases[i][vect[0].second],vBases[i][vect[2].second]))//verifica se houve colisão
                 {
+                    //define a normal utilizando o perproduct apresentado nesse link https://www.metanetsoftware.com/technique/tutorialA.html#arvo
                     float ny = -(vBases[i][vect[0].second].x - vBases[i][vect[2].second].x);
                     float nx = vBases[i][vect[0].second].y - vBases[i][vect[2].second].y;
                     vertice auxV1 = {nx+vBases[i][vect[0].second].x,ny+vBases[i][vect[0].second].y,0};
                     vertice auxV2 = {-(nx+vBases[i][vect[0].second].x),-(ny+vBases[i][vect[0].second].y),0};
-                    if(distance2vertices(auxV1,centros[i]) < distance2vertices(auxV2,centros[i]))
+                    if(distancia2vertices(auxV1,centros[i]) < distancia2vertices(auxV2,centros[i]))//esta condição serve para definir o sentido da normal
                     {
                         nx *= -1;
                         ny *= -1;
@@ -396,7 +424,7 @@ void colision()
                     break;
                 }
             }
-            else if(vect[0].first <= 0.5)
+            else if(vect[0].first <= 0.5)//se nao houve colisão com as arestas, testa se ocorreu colisão com o vertice mais proximo
             {
                 calculaVetorNovo(sphereX-vBases[i][vect[0].second].x,sphereY-vBases[i][vect[0].second].y);
                 colide = true;
@@ -425,7 +453,9 @@ void idle ()
     if( frameTime <= desiredFrameTime)
         return;
 
+    //verifica colisão com o ambiente
     colisionAmbient();
+    //esta condição impede que ocorra mais de uma resposta para a mesma colisão da esfera com um determinado prisma, já que impede de verificar colisão ate passar alguns milisegundos
     if(colide)
     {
         if(t-timet > 0.09)
@@ -433,18 +463,18 @@ void idle ()
     }
     else
     {
-       colision();
+        colision();//verifica colisão da esfera com os prismas
     }
 
     float step = 1;
-    if(launched)
+    if(launched)//verifica se a esfera foi lançada, se caso ocorrer movimenta a esfera com a direção e velocidades apropriados
     {
-        velX = dx*velInicial*2;
-        velY = dy*velInicial*2;
+        velX = dx*velInicial*intensidade;
+        velY = dy*velInicial*intensidade;
         sphereX+=velX* (step / desiredFPS);
         sphereY+=velY* (step / desiredFPS);
     }
-    else
+    else//determina direção do disparador e posição inicial da esfera caso nao seja lançada
     {
         dx = cos((direction+90)*0.0174533);
         dy = sin((direction+90)*0.0174533);
